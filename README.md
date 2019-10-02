628fc3ca1f822c5d3455ec3cbbd0a1c448c91224
Brandon Evans <brandon@brandonevans.ca>
2019-10-02 14:06:50 -0600

    Add --embed-in-xcode flag for Xcode 11 support
    https://github.com/apollographql/xcode-graphql/issues/23

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ Run the following command in your terminal:
 ./setup.sh
 ```
 
+Xcode 11 has changed where language specifications are stored, and no longer looks for them in `~/Library/Developer/Xcode`. A workaround is provided which installs the necessary files within Xcode itself where they'll be loaded. It goes without saying that changing the contents of Xcode's app bundle is a fragile solution that is likely to break in the future.
+
+```
+./setup.sh --embed-in-xcode /Applications/Xcode-11.app
+```
+
 ### Manual installation
 
 Please note that if you are running Xcode 8 the `Plug-ins` and `Specifications` directories might not exist.
--- setup.sh
+++ setup.sh
@@ -2,6 +2,22 @@
 
 set -o xtrace
 
+# Embed inside Xcode 11
+# https://github.com/apollographql/xcode-graphql/issues/23#issuecomment-534634008
+# Example: ./setup.sh --embed-in-xcode /Applications/Xcode-11.app
+if [ "$1" == "--embed-in-xcode" ]; then
+	plist_dir="$2/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata"
+	spec_dir="$2/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications"
+
+	if [ -d "$2" ] && [ -d "$plist_dir" ] && [ -d "$spec_dir" ]; then
+		cp Xcode.SourceCodeLanguage.GraphQL.plist $plist_dir
+		cp GraphQL.xclangspec $spec_dir
+	else
+		echo '⚠️ Must supply a valid path to an Xcode.app after the --embed-in-xcode flag. Either the provided path was invalid or that Xcode doesn'\''t contain the expected directory to install within.'
+		exit 1
+	fi
+fi
+
 plugins_dir=~/Library/Developer/Xcode/Plug-ins/
 spec_dir=~/Library/Developer/Xcode/Specifications
 
